d7b803ca88294d56f73a09271409593c4a663540
rita lanca <rita.lanca17@gmail.com>
2024-11-05 18:12:15 +0000

    add comments

--- data/database.js
+++ data/database.js
@@ -1,10 +1,12 @@
 import { MongoClient } from 'mongodb';
 
+// environment variables
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
+// connection string created dynamic for each environment this appplication is running
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
