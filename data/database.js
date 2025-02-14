738f4eed4840a1624626adbc722fab5803deec19
rita lanca <rita.lanca17@gmail.com>
2025-02-14 11:09:04 +0000

    Docker conatiners and github actions: initial commit
    Add new information to setup the project that will be explaining docker in github actions

--- data/database.js
+++ data/database.js
@@ -1,13 +1,14 @@
 import { MongoClient } from 'mongodb';
 
 // environment variables
+const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
 const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
 // connection string created dynamic for each environment this appplication is running
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
